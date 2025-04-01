9718ff1cbd25b8df16f6a0a2548d3288cb85496d
ArtemK787 <artem.kliuchko@studentfr.ch>
2025-04-01 16:38:02 +0200

    Commentaires ajoutés

--- src/services/ListePersonne_Dynamique.java
+++ src/services/ListePersonne_Dynamique.java
@@ -8,43 +8,43 @@ public class ListePersonne_Dynamique {
 
     public ListePersonne_Dynamique() {
 
-        // taille initialie du tableau est 0
+        // Taille initialie du tableau est 0
 
         this.personnes = new Personne[0];
     }
 
     public int getNombre() {
 
-        // return la taille du tableau
+        // Return la taille du tableau
 
         return this.personnes.length;
     }
 
     public void vider() {
 
-        // vider le tableau en mettant la taille à 0
+        // Vider le tableau en mettant la taille à 0
 
         this.personnes = new Personne[0];
     }
 
     public boolean ajouter(Personne p) {
 
-        // variable de vérification
+        // Variable de vérification
 
         boolean ajoutReussi = false;
 
-        // mettre les anciennes valeurs dans le nouveau tableau
+        // Mettre les anciennes valeurs dans le nouveau tableau
 
         Personne[] listeAjout = new Personne[this.personnes.length + 1];
         for (int i = 0; i < this.personnes.length; i++) {
             listeAjout[i] = this.personnes[i];
         }
 
-        // remplacer les cellules vides de nouveau tableau par la variable
+        // Remplacer les cellules vides de nouveau tableau par la variable
 
         listeAjout[listeAjout.length - 1] = p;
 
-        // remplacer le tableau en paramètre par celui qui a supprimé la variable
+        // Remplacer le tableau en paramètre par celui qui a supprimé la variable
 
         this.personnes = listeAjout;
         ajoutReussi = true;
@@ -52,11 +52,14 @@ public class ListePersonne_Dynamique {
     }
 
     public boolean supprimer(Personne p) {
+
         // Variable de vérification
+
         boolean suppressionReussie = false;
         int trouveIndex = -1;
 
         // Trouver index à supprimer
+
         for (int i = 0; i < this.personnes.length; i++) {
             if (this.personnes[i] == p) {
                 trouveIndex = i;
@@ -64,12 +67,16 @@ public class ListePersonne_Dynamique {
             }
         }
 
-        // Si l'élément n'a pas été trouvé, retourner false
+        // Vérifier que trouveIndex != -1 (valeur intiale) pour procéder à la suppresion
+
         if (trouveIndex != -1) {
+
             // Diminuer la taille de tableau par rapport à l'élément supprimé
+
             Personne[] listeSupprime = new Personne[this.personnes.length - 1];
 
             // Copier les éléments restants dans le nouveau tableau
+
             for (int i = 0, j = 0; i < this.personnes.length; i++) {
                 if (i != trouveIndex) {
                     listeSupprime[j] = this.personnes[i];
@@ -78,6 +85,7 @@ public class ListePersonne_Dynamique {
             }
 
             // Remplacer ancient tableau par le nouveau
+
             this.personnes = listeSupprime;
 
             suppressionReussie = true;
@@ -92,6 +100,9 @@ public class ListePersonne_Dynamique {
         if (this.personnes.length != 0) {
             System.out.println("Liste de personnes :");
             for (int i = 0; i < this.personnes.length; i++) {
+
+                // i + 1 pour afficher les index à partir de 1
+
                 System.out.println((i + 1) + ". " + this.personnes[i]);
             }
             System.out.println("La taille du tableau : " + this.personnes.length);
